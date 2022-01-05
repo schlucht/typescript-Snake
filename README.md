b99646e325a21a64f61e82c29e82932777638c84
schlucht <lothi@schmidschlucht.ch>
2022-01-05 09:33:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-cx4qjl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-cx4qjl)
---
+++ index.ts
@@ -0,0 +1,11 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const CANVAS: HTMLElement = document.getElementById('canvas');
+const SIZE: number = 10;
+
+const WIDTH: number = 500;
+const HEIGHT: number = 300;
+const ROWS: number = CANVAS.clientHeight / SIZE;
+const COLS: number = CANVAS.clientWidth / SIZE;
