5d5eaeefc983208ac0f97bedfe70a67970b51b1a
schlucht <web@jagolo.ch>
2022-01-08 15:09:32 +0100

    Create new Snake Project

--- index.ts
+++
@@ -1,9 +0,0 @@
-// Import stylesheets
-import './style.css';
-
-import { CreateCanvas } from './ts/createCanvas';
-import { DrawPlayGround } from './ts/drawPlayground';
-
-const canvas = new CreateCanvas('');
-const playground = new DrawPlayGround(canvas.Canvas, 10);
-playground.draw();
